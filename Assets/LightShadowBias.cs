ab626e950a384d6649fdaea4c05b7d4b7684194b
Fixer4013 <fixer4013@gmail.com>
2021-01-25 18:27:02 +0100

    Some ligh changes

---
+++ Assets/LightShadowBias.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightShadowBias : MonoBehaviour
+{
+    void Start()
+    {
+        var lightCom = GetComponent<Light>();
+        //lightCom.shadowBias = -0.11f;
+
+    }
+
+}
