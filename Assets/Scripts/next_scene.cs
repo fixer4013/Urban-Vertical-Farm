af2aea8d719c2ccc2778053288857f56043ae4cd
mujuik <63315230+mujuik@users.noreply.github.com>
2021-01-25 01:27:29 +0000

    late night work
    so the models are in side the building;
    there is a kind of door in the back of the building to go to the next scene that already works but needs to be more visible, the part for the vertical farm is done but looks a bit dull but I dont know how to fix

---
+++ Assets/Scripts/next_scene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class next_scene : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other){
+        SceneManager.LoadScene(1);
+        Debug.Log("yo it worksss");
+    }
+        
+    
+}
